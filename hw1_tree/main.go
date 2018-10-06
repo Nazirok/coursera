e98766f4a12f772a9695c6e3b865fa4f06296702
Nazirok <nazir_06@mail.ru>
2018-10-06 21:52:14 +0300

    del testdata

--- hw1_tree/main.go
+++ hw1_tree/main.go
@@ -24,13 +24,11 @@ const (
 
 func main() {
 	out := os.Stdout
-	//if !(len(os.Args) == 2 || len(os.Args) == 3) {
-	//	panic("usage go run main.go . [-f]")
-	//}
-	//path := os.Args[1]
-	path := `C:\GoPath\src\github.com\coursera\hw1_tree\testdata`
-	//printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
-	printFiles := true
+	if !(len(os.Args) == 2 || len(os.Args) == 3) {
+		panic("usage go run main.go . [-f]")
+	}
+	path := os.Args[1]
+	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
 	err := dirTree(out, path, printFiles)
 	if err != nil {
 		panic(err.Error())
