29278cd1025b399b8576ab5078dc97df972c48c4
Nazirok <nazir_06@mail.ru>
2018-10-08 23:06:22 +0300

    first version

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -1,3 +1,27 @@
 package main
 
-// сюда писать код
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	DataSignerCrc32("sdf")
+	first := (SingleHash(strconv.Itoa(0)))
+	fmt.Println(first)
+	MultiHash(first)
+}
+
+func SingleHash(in, out chan interface{}) string {
+	md5Crc32 := DataSignerCrc32(DataSignerMd5(data))
+	crc32 := DataSignerCrc32(data)
+	return crc32 + "~" + md5Crc32
+}
+
+func MultiHash(in, out chan interface{}) string {
+	for i:=0; i<=5; i++ {
+		d := strconv.Itoa(i) + data
+		fmt.Println(i, DataSignerCrc32(d))
+	}
+	return ""
+}
