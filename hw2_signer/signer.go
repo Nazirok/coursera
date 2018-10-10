a6464699a04287a99f9add245dc77f93ef8131cc
Nazirok <nazir_06@mail.ru>
2018-10-10 22:51:26 +0300

    intermediate

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -3,29 +3,77 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"sync"
 )
 
 func main() {
-	DataSignerCrc32("sdf")
-	first := (SingleHash(strconv.Itoa(0)))
-	fmt.Println(first)
-	MultiHash(first)
-}
-
-func ExecutePipeline(job...func(in, out chan interface{})) {
-
-}
-
-func SingleHash(in, out chan interface{}) string {
-	md5Crc32 := DataSignerCrc32(DataSignerMd5(data))
-	crc32 := DataSignerCrc32(data)
-	return crc32 + "~" + md5Crc32
-}
-
-func MultiHash(in, out chan interface{}) string {
-	for i:=0; i<=5; i++ {
-		d := strconv.Itoa(i) + data
-		fmt.Println(i, DataSignerCrc32(d))
+	f := func(in, out chan interface{}) {
+		out <- "0"
 	}
-	return ""
+	ExecutePipeline(f, SingleHash, MultiHash, CombineResults)
+	//time.Sleep(10 * time.Second)
+}
+
+func ExecutePipeline(jobs ...func(in, out chan interface{})) {
+	in := make(chan interface{})
+	out := make(chan interface{})
+	wg := &sync.WaitGroup{}
+	for _, job := range jobs {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, in, out chan interface{}) {
+			defer wg.Done()
+			job(in, out)
+		}(wg, in, out)
+		in = out
+		out = make(chan interface{})
+	}
+	wg.Wait()
+}
+
+func SingleHash(in, out chan interface{}) {
+	fmt.Println("SingleHash")
+	for v := range in {
+		data, ok := v.(string)
+
+		if !ok {
+			continue
+		}
+
+		md5Crc32 := DataSignerCrc32(DataSignerMd5(data))
+		crc32 := DataSignerCrc32(data)
+		out <- crc32 + "~" + md5Crc32
+
+	}
+	//close(out)
+}
+
+func MultiHash(in, out chan interface{}) {
+	fmt.Println("MultiHash")
+
+	for v := range in {
+		data, ok := v.(string)
+
+		if !ok {
+			continue
+		}
+
+		var toOut string
+		for i := 0; i <= 5; i++ {
+			d := strconv.Itoa(i) + data
+			toOut += DataSignerCrc32(d)
+
+		}
+		out <- toOut
+	}
+	//close(out)
+}
+
+func CombineResults(in, out chan interface{}) {
+	fmt.Println("Combine")
+
+	for v := range in {
+		fmt.Println(v)
+		out <- v
+	}
+	//close(out)
 }
