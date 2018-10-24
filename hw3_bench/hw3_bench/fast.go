d820f0f8b3abf417eb40a90b54f8b72de5c0f4e3
Nazirok <nazir_06@mail.ru>
2018-10-24 23:09:34 +0300

    add easyjson

--- hw3_bench/hw3_bench/fast.go
+++ hw3_bench/hw3_bench/fast.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
 	"io"
 	"os"
 	"regexp"
 	"strings"
+	json "encoding/json"
+	easyjson "github.com/mailru/easyjson"
+	jlexer "github.com/mailru/easyjson/jlexer"
+	jwriter "github.com/mailru/easyjson/jwriter"
+)
+
+// suppress unused package warning
+var (
+	_ *json.RawMessage
+	_ *jlexer.Lexer
+	_ *jwriter.Writer
+	_ easyjson.Marshaler
 )
 
 // вам надо написать более быструю оптимальную этой функции
 
+//easyjson:json
 type User struct {
 	Name     string   `json:"name"`
 	Phone    string   `json:"phone"`
@@ -34,53 +46,218 @@ func FastSearch(out io.Writer) {
 	seenBrowsers := make(map[string]struct{})
 	foundUsers := ""
 
-	users := make([]User, 0)
+	var i int
+
 	for scanner.Scan() {
 		user := User{}
-		// fmt.Printf("%v %v\n", err, line)
-		err := json.Unmarshal([]byte(scanner.Text()), &user)
+		err := user.UnmarshalJSON([]byte(scanner.Text()))
+
 		if err != nil {
 			panic(err)
 		}
 
-		users = append(users, user)
-	}
-
-	for i, user := range users {
-
 		isAndroid := false
 		isMSIE := false
 
-		browsers := user.Browsers
-
-		for _, browser := range browsers {
-			android := strings.Contains(browser, "Android")
-			msie := strings.Contains(browser, "MSIE")
-
-			if android || msie {
-				if android {
-					isAndroid = true
+		for _, browser := range user.Browsers {
+			if strings.Contains(browser, "Android") {
+				isAndroid = true
+				if _, ok := seenBrowsers[browser]; !ok {
+					seenBrowsers[browser] = struct{}{}
 				}
-
-				if msie {
-					isMSIE = true
-				}
-
+			}
+			if strings.Contains(browser, "MSIE") {
+				isMSIE = true
 				if _, ok := seenBrowsers[browser]; !ok {
 					seenBrowsers[browser] = struct{}{}
 				}
 			}
 		}
 
-		if !(isAndroid && isMSIE) {
-			continue
+		if isAndroid && isMSIE {
+			email := r.ReplaceAllString(user.Email, " [at] ")
+			foundUsers += fmt.Sprintf("[%d] %s <%s>\n", i, user.Name, email)
 		}
 
-		// log.Println("Android and MSIE user:", user["name"], user["email"])
-		email := r.ReplaceAllString(user.Email, " [at] ")
-		foundUsers += fmt.Sprintf("[%d] %s <%s>\n", i, user.Name, email)
+		i++
 	}
 
 	fmt.Fprintln(out, "found users:\n"+foundUsers)
 	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
+}
+
+func easyjson797c97daDecodeGithubComCourseraTemp(in *jlexer.Lexer, out *User) {
+	isTopLevel := in.IsStart()
+	if in.IsNull() {
+		if isTopLevel {
+			in.Consumed()
+		}
+		in.Skip()
+		return
+	}
+	in.Delim('{')
+	for !in.IsDelim('}') {
+		key := in.UnsafeString()
+		in.WantColon()
+		if in.IsNull() {
+			in.Skip()
+			in.WantComma()
+			continue
+		}
+		switch key {
+		case "name":
+			out.Name = string(in.String())
+		case "phone":
+			out.Phone = string(in.String())
+		case "browsers":
+			if in.IsNull() {
+				in.Skip()
+				out.Browsers = nil
+			} else {
+				in.Delim('[')
+				if out.Browsers == nil {
+					if !in.IsDelim(']') {
+						out.Browsers = make([]string, 0, 4)
+					} else {
+						out.Browsers = []string{}
+					}
+				} else {
+					out.Browsers = (out.Browsers)[:0]
+				}
+				for !in.IsDelim(']') {
+					var v1 string
+					v1 = string(in.String())
+					out.Browsers = append(out.Browsers, v1)
+					in.WantComma()
+				}
+				in.Delim(']')
+			}
+		case "company":
+			out.Company = string(in.String())
+		case "country":
+			out.Country = string(in.String())
+		case "email":
+			out.Email = string(in.String())
+		case "job":
+			out.Job = string(in.String())
+		default:
+			in.SkipRecursive()
+		}
+		in.WantComma()
+	}
+	in.Delim('}')
+	if isTopLevel {
+		in.Consumed()
+	}
+}
+func easyjson797c97daEncodeGithubComCourseraTemp(out *jwriter.Writer, in User) {
+	out.RawByte('{')
+	first := true
+	_ = first
+	{
+		const prefix string = ",\"name\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		out.String(string(in.Name))
+	}
+	{
+		const prefix string = ",\"phone\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		out.String(string(in.Phone))
+	}
+	{
+		const prefix string = ",\"browsers\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		if in.Browsers == nil && (out.Flags&jwriter.NilSliceAsEmpty) == 0 {
+			out.RawString("null")
+		} else {
+			out.RawByte('[')
+			for v2, v3 := range in.Browsers {
+				if v2 > 0 {
+					out.RawByte(',')
+				}
+				out.String(string(v3))
+			}
+			out.RawByte(']')
+		}
+	}
+	{
+		const prefix string = ",\"company\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		out.String(string(in.Company))
+	}
+	{
+		const prefix string = ",\"country\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		out.String(string(in.Country))
+	}
+	{
+		const prefix string = ",\"email\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		out.String(string(in.Email))
+	}
+	{
+		const prefix string = ",\"job\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		out.String(string(in.Job))
+	}
+	out.RawByte('}')
+}
+
+// MarshalJSON supports json.Marshaler interface
+func (v User) MarshalJSON() ([]byte, error) {
+	w := jwriter.Writer{}
+	easyjson797c97daEncodeGithubComCourseraTemp(&w, v)
+	return w.Buffer.BuildBytes(), w.Error
+}
+
+// MarshalEasyJSON supports easyjson.Marshaler interface
+func (v User) MarshalEasyJSON(w *jwriter.Writer) {
+	easyjson797c97daEncodeGithubComCourseraTemp(w, v)
+}
+
+// UnmarshalJSON supports json.Unmarshaler interface
+func (v *User) UnmarshalJSON(data []byte) error {
+	r := jlexer.Lexer{Data: data}
+	easyjson797c97daDecodeGithubComCourseraTemp(&r, v)
+	return r.Error()
+}
+
+// UnmarshalEasyJSON supports easyjson.Unmarshaler interface
+func (v *User) UnmarshalEasyJSON(l *jlexer.Lexer) {
+	easyjson797c97daDecodeGithubComCourseraTemp(l, v)
 }
