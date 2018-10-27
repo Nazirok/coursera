32aed38298c13f173f7cbe12f7a2a49ba51d33bd
Nazirok <nazir_06@mail.ru>
2018-10-27 13:24:32 +0300

    final version

--- hw3_bench/hw3_bench/fast.go
+++ hw3_bench/hw3_bench/fast.go
@@ -26,12 +26,8 @@ var (
 //easyjson:json
 type User struct {
 	Name     string   `json:"name"`
-	Phone    string   `json:"phone"`
 	Browsers []string `json:"browsers"`
-	Company  string   `json:"company"`
-	Country  string   `json:"country"`
 	Email    string   `json:"email"`
-	Job      string   `json:"job"`
 }
 
 func FastSearch(out io.Writer) {
@@ -98,6 +94,7 @@ func FastSearch(out io.Writer) {
 		}
 
 		if isAndroid && isMSIE {
+			p := strings.Split(user.Email, "@")
 			io.WriteString(out, "[")
 			io.WriteString(out, strconv.Itoa(i))
 			io.WriteString(out, "]")
@@ -105,7 +102,7 @@ func FastSearch(out io.Writer) {
 			io.WriteString(out, user.Name)
 			io.WriteString(out, " ")
 			io.WriteString(out, "<")
-			io.WriteString(out, strings.Replace(user.Email, "@", " [at] ", -1))
+			io.WriteString(out, p[0]+" [at] "+p[1])
 			io.WriteString(out, ">")
 			io.WriteString(out, "\n")
 		}
@@ -119,7 +116,7 @@ func FastSearch(out io.Writer) {
 
 }
 
-func easyjson797c97daDecodeGithubComCourseraTemp(in *jlexer.Lexer, out *User) {
+func easyjson785d9294DecodeGithubComCourseraHw3BenchHw3BenchData(in *jlexer.Lexer, out *User) {
 	isTopLevel := in.IsStart()
 	if in.IsNull() {
 		if isTopLevel {
@@ -140,8 +137,6 @@ func easyjson797c97daDecodeGithubComCourseraTemp(in *jlexer.Lexer, out *User) {
 		switch key {
 		case "name":
 			out.Name = string(in.String())
-		case "phone":
-			out.Phone = string(in.String())
 		case "browsers":
 			if in.IsNull() {
 				in.Skip()
@@ -165,14 +160,8 @@ func easyjson797c97daDecodeGithubComCourseraTemp(in *jlexer.Lexer, out *User) {
 				}
 				in.Delim(']')
 			}
-		case "company":
-			out.Company = string(in.String())
-		case "country":
-			out.Country = string(in.String())
 		case "email":
 			out.Email = string(in.String())
-		case "job":
-			out.Job = string(in.String())
 		default:
 			in.SkipRecursive()
 		}
@@ -183,7 +172,7 @@ func easyjson797c97daDecodeGithubComCourseraTemp(in *jlexer.Lexer, out *User) {
 		in.Consumed()
 	}
 }
-func easyjson797c97daEncodeGithubComCourseraTemp(out *jwriter.Writer, in User) {
+func easyjson785d9294EncodeGithubComCourseraHw3BenchHw3BenchData(out *jwriter.Writer, in User) {
 	out.RawByte('{')
 	first := true
 	_ = first
@@ -196,16 +185,6 @@ func easyjson797c97daEncodeGithubComCourseraTemp(out *jwriter.Writer, in User) {
 			out.RawString(prefix)
 		}
 		out.String(string(in.Name))
-	}
-	{
-		const prefix string = ",\"phone\":"
-		if first {
-			first = false
-			out.RawString(prefix[1:])
-		} else {
-			out.RawString(prefix)
-		}
-		out.String(string(in.Phone))
 	}
 	{
 		const prefix string = ",\"browsers\":"
@@ -229,26 +208,6 @@ func easyjson797c97daEncodeGithubComCourseraTemp(out *jwriter.Writer, in User) {
 		}
 	}
 	{
-		const prefix string = ",\"company\":"
-		if first {
-			first = false
-			out.RawString(prefix[1:])
-		} else {
-			out.RawString(prefix)
-		}
-		out.String(string(in.Company))
-	}
-	{
-		const prefix string = ",\"country\":"
-		if first {
-			first = false
-			out.RawString(prefix[1:])
-		} else {
-			out.RawString(prefix)
-		}
-		out.String(string(in.Country))
-	}
-	{
 		const prefix string = ",\"email\":"
 		if first {
 			first = false
@@ -258,39 +217,29 @@ func easyjson797c97daEncodeGithubComCourseraTemp(out *jwriter.Writer, in User) {
 		}
 		out.String(string(in.Email))
 	}
-	{
-		const prefix string = ",\"job\":"
-		if first {
-			first = false
-			out.RawString(prefix[1:])
-		} else {
-			out.RawString(prefix)
-		}
-		out.String(string(in.Job))
-	}
 	out.RawByte('}')
 }
 
 // MarshalJSON supports json.Marshaler interface
 func (v User) MarshalJSON() ([]byte, error) {
 	w := jwriter.Writer{}
-	easyjson797c97daEncodeGithubComCourseraTemp(&w, v)
+	easyjson785d9294EncodeGithubComCourseraHw3BenchHw3BenchData(&w, v)
 	return w.Buffer.BuildBytes(), w.Error
 }
 
 // MarshalEasyJSON supports easyjson.Marshaler interface
 func (v User) MarshalEasyJSON(w *jwriter.Writer) {
-	easyjson797c97daEncodeGithubComCourseraTemp(w, v)
+	easyjson785d9294EncodeGithubComCourseraHw3BenchHw3BenchData(w, v)
 }
 
 // UnmarshalJSON supports json.Unmarshaler interface
 func (v *User) UnmarshalJSON(data []byte) error {
 	r := jlexer.Lexer{Data: data}
-	easyjson797c97daDecodeGithubComCourseraTemp(&r, v)
+	easyjson785d9294DecodeGithubComCourseraHw3BenchHw3BenchData(&r, v)
 	return r.Error()
 }
 
 // UnmarshalEasyJSON supports easyjson.Unmarshaler interface
 func (v *User) UnmarshalEasyJSON(l *jlexer.Lexer) {
-	easyjson797c97daDecodeGithubComCourseraTemp(l, v)
+	easyjson785d9294DecodeGithubComCourseraHw3BenchHw3BenchData(l, v)
 }
