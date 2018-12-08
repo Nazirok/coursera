02376716d837d6b5ee70338ba27d332af75a5a87
Nazirok <nazir_06@mail.ru>
2018-12-08 21:00:25 +0300

    hw6 firdt

--- hw6_db_explorer/db_explorer.go
+++ hw6_db_explorer/db_explorer.go
@@ -2,3 +2,48 @@ package main
 
 // тут вы пишете код
 // обращаю ваше внимание - в этом задании запрещены глобальные переменные
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"fmt"
+	"net/http"
+)
+
+func NewDbExplorer(db *sql.DB) (*dbExplorer, error) {
+	rows, err := db.Query("SHOW TABLES")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var table string
+		tableRows := make(map[string][]string)
+		rows.Scan(&table)
+		fmt.Println(table)
+		fields, err := db.Query("SHOW FULL COLUMNS FROM `?`", table)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &dbExplorer{DB: db}, nil
+}
+
+type dbExplorer struct {
+	DB     *sql.DB
+	Tables map[string][]string
+}
+
+type tableField struct {
+	Name string
+	Type string
+	Null string
+	Pri  string
+}
+
+func (d *dbExplorer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+}
--- hw6_db_explorer/main.go
+++ hw6_db_explorer/main.go
@@ -4,8 +4,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
-	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -14,7 +12,7 @@ var (
 	// DSN это соединение с базой
 	// вы можете изменить этот на тот который вам нужен
 	// docker run -p 3306:3306 -v $(PWD):/docker-entrypoint-initdb.d -e MYSQL_ROOT_PASSWORD=1234 -e MYSQL_DATABASE=golang -d mysql
-	DSN = "root@tcp(localhost:3306)/golang2017?charset=utf8"
+	DSN = "root:1234@tcp(192.168.99.100:3306)/golang?charset=utf8"
 	// DSN = "coursera:5QPbAUufx7@tcp(localhost:3306)/coursera?charset=utf8"
 )
 
@@ -25,11 +23,12 @@ func main() {
 		panic(err)
 	}
 
-	handler, err := NewDbExplorer(db)
-	if err != nil {
-		panic(err)
-	}
+	NewDbExplorer(db)
+	//handler, err := NewDbExplorer(db)
+	//if err != nil {
+	//	panic(err)
+	//}
 
-	fmt.Println("starting server at :8082")
-	http.ListenAndServe(":8082", handler)
+	//fmt.Println("starting server at :8082")
+	//http.ListenAndServe(":8082", handler)
 }
