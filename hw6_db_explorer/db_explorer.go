c2d4423e9ecde9accb4b2725c0f9dee609f5b2ab
Nazirok <nazir_06@mail.ru>
2018-12-12 23:08:01 +0300

    get limit offset

--- hw6_db_explorer/db_explorer.go
+++ hw6_db_explorer/db_explorer.go
@@ -11,9 +11,14 @@ import (
 	"encoding/json"
 	"net/http"
 	"strings"
+	"fmt"
 )
 
 func NewDbExplorer(db *sql.DB) (*dbExplorer, error) {
+	defer func() {
+		fmt.Println("Conns: ", db.Stats().OpenConnections)
+	}()
+	fmt.Println("Conns beign: ", db.Stats().OpenConnections)
 	tablesData := make(map[string][]map[string]interface{})
 	rows, err := db.Query("SHOW TABLES")
 	if err != nil {
@@ -61,14 +66,6 @@ func NewDbExplorer(db *sql.DB) (*dbExplorer, error) {
 			tablesData[table] = append(tablesData[table], entry)
 		}
 	}
-
-	//jsonData, err := json.Marshal(tablesData)
-	//if err != nil {
-	//	fmt.Println("marshal", err)
-	//}
-	//
-	//fmt.Printf("%s\n", jsonData)
-
 	return &dbExplorer{DB: db, Tables: tablesData}, nil
 }
 
@@ -90,9 +87,13 @@ func (d *dbExplorer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 	} else {
 		p := strings.Split(r.URL.Path, "/")[1:]
-		//l := len(p)
 		if !d.tableExist(p[0]) {
 			writeResponse(w, resp{"error": "unknown table"}, http.StatusNotFound)
+			return
+		}
+		if len(p) == 1 && r.Method == http.MethodGet {
+			d.handleGetAll(w, r, p[0])
+			return
 		}
 	}
 }
@@ -106,6 +107,63 @@ func (d *dbExplorer) handleBase(w http.ResponseWriter, r *http.Request) {
 		"error": "",
 		"response": map[string][]string{
 			"tables": out,
+		},
+	}
+	writeResponse(w, res, http.StatusOK)
+}
+
+func (d *dbExplorer) handleGetAll(w http.ResponseWriter, r *http.Request, table string) {
+	var limit, offset string
+	limit = r.URL.Query().Get("limit")
+	if limit == "" {
+		limit = "5"
+	}
+	offset = r.URL.Query().Get("offset")
+	if offset == "" {
+		offset = "0"
+	}
+	rows, err := d.DB.Query("SELECT * FROM " + table + " LIMIT " + limit + " OFFSET " + offset)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	columns, err := rows.Columns()
+	count := len(columns)
+	values := make([]interface{}, count)
+	scanArgs := make([]interface{}, count)
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+	out := make([]map[string]interface{}, 0, 5)
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		entry := make(map[string]interface{})
+		for i, col := range columns {
+			v := values[i]
+			switch v.(type) {
+			case []byte:
+				entry[col] = string(v.([]byte))
+			case string:
+				entry[col] = v.(string)
+			case int:
+				entry[col] = v.(int)
+			case bool:
+				entry[col] = v.(bool)
+			case nil:
+				entry[col] = nil
+			}
+		}
+		out = append(out, entry)
+	}
+	res := resp{
+		"response": map[string]interface{}{
+			"records": out,
 		},
 	}
 	writeResponse(w, res, http.StatusOK)
