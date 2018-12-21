33360bf512e2726a11c2a4d10fef775f1b4d8d93
Nazirok <nazirbavizh@gmail.com>
2018-12-21 16:05:02 +0300

    refactor

--- hw7_microservice/service.go
+++ hw7_microservice/service.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"net"
-	"encoding/json"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // тут вы пишете код
 // обращаю ваше внимание - в этом задании запрещены глобальные переменные
 
 type (
+	MicroService struct {
+		acl   map[string][]string
+		admin *AdminService
+		biz   *BizService
+	}
+
 	AdminService struct {
 	}
 
@@ -19,12 +28,40 @@ type (
 	}
 )
 
-func NewServiceManager() *AdminService {
-	return &AdminService{}
+func NewMicroService(acl map[string][]string) *MicroService {
+	return &MicroService{
+		acl:   acl,
+		admin: &AdminService{},
+		biz:   &BizService{},
+	}
 }
 
-func NewBizService() *BizService {
-	return &BizService{}
+func (m *MicroService) GetAdminService() *AdminService {
+	return m.admin
+}
+
+func (m *MicroService) GetBizService() *BizService {
+	return m.biz
+}
+
+func (m *MicroService) authInterceptor(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+) (interface{}, error) {
+	md, _ := metadata.FromIncomingContext(ctx)
+	consumer := md.Get("consumer")
+	list, ok := m.acl[consumer[0]]
+	if !ok {
+		return nil, status.Error(codes.Unauthenticated, "unknown consumer")
+	}
+	fmt.Println(list)
+	reply, err := handler(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return reply, nil
 }
 
 func StartMyMicroservice(ctx context.Context, conn string, acl string) error {
@@ -32,15 +69,17 @@ func StartMyMicroservice(ctx context.Context, conn string, acl string) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println(access)
+	micro := NewMicroService(access)
 	lis, err := net.Listen("tcp", conn)
 	if err != nil {
-		fmt.Errorf("can`t listen port %s", err)
+		return fmt.Errorf("can`t listen port %s", err)
 	}
 
-	server := grpc.NewServer()
-	RegisterAdminServer(server, NewServiceManager())
-	RegisterBizServer(server, NewBizService())
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(micro.authInterceptor),
+	)
+	RegisterAdminServer(server, micro.GetAdminService())
+	RegisterBizServer(server, micro.GetBizService())
 	go func() {
 		server.Serve(lis)
 	}()
