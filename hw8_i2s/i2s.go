c208262302d7079932334892ece8341f90580b08
Nazirok <nazirbavizh@gmail.com>
2019-01-18 16:58:26 +0300

    first

--- hw8_i2s/i2s.go
+++ hw8_i2s/i2s.go
@@ -1,7 +1,17 @@
 package main
 
-/*
+import (
+	"reflect"
+	"fmt"
+)
+
 func i2s(data interface{}, out interface{}) error {
-	// todo
+	v := reflect.ValueOf(data)
+	fmt.Println(reflect.TypeOf(data).Kind())
+	switch reflect.TypeOf(data).Kind() {
+	case reflect.Map:
+		fmt.Println("Struct")
+	}
+	return nil
 }
-*/
+
