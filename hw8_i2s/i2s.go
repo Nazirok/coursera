6827e45ee6c5481611cdbaa312f36a6e9942376d
Nazirok <nazir_06@mail.ru>
2019-07-28 18:13:31 +0300

    finish

--- hw8_i2s/i2s.go
+++ hw8_i2s/i2s.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
 )
 
 func i2s(data interface{}, out interface{}) error {
 	vd := reflect.ValueOf(data)
 	vo := reflect.ValueOf(out)
+	if vo.Kind() != reflect.Ptr {
+		return errors.New("out type must be ptr")
+	}
 	if err := set(vd, vo); err != nil {
 		return err
 	}
@@ -29,33 +33,44 @@ func set(mv, sv reflect.Value) error {
 			sv.SetInt(mv.Int())
 		case reflect.Float64:
 			sv.SetInt(int64(mv.Float()))
+		default:
+			return errors.New(fmt.Sprintf("type %s can`t set to %s", mv.Kind(), sv.Kind()))
 		}
 	case reflect.String:
+		if mv.Kind() != reflect.String {
+			return errors.New(fmt.Sprintf("type %s can`t set to %s", mv.Kind(), sv.Kind()))
+		}
 		sv.SetString(mv.String())
 	case reflect.Bool:
+		if mv.Kind() != reflect.Bool {
+			return errors.New(fmt.Sprintf("type %s can`t set to %s", mv.Kind(), sv.Kind()))
+		}
 		sv.SetBool(mv.Bool())
 	case reflect.Slice:
+		if mv.Kind() != reflect.Slice {
+			return errors.New(fmt.Sprintf("type %s can`t set to %s", mv.Kind(), sv.Kind()))
+		}
 		slice := reflect.MakeSlice(sv.Type(), mv.Len(), mv.Len())
-		if mv.Kind() == reflect.Slice {
-			for i := 0; i < mv.Len(); i++ {
-				if err := set(mv.Index(i).Elem(), slice.Index(i)); err != nil {
-					return err
-				}
+		for i := 0; i < mv.Len(); i++ {
+			if err := set(mv.Index(i).Elem(), slice.Index(i)); err != nil {
+				return err
 			}
 		}
 		sv.Set(slice)
 	case reflect.Struct:
-		switch mv.Kind() {
-		case reflect.Map:
-			for _, key := range mv.MapKeys() {
-				ssv := sv.FieldByName(key.String())
-				mmv := mv.MapIndex(key).Elem()
-				if err := set(mmv, ssv); err != nil {
-					return err
-				}
+		if mv.Kind() != reflect.Map {
+			return errors.New(fmt.Sprintf("type %s can`t set to %s", mv.Kind(), sv.Kind()))
+		}
+		for _, key := range mv.MapKeys() {
+			ssv := sv.FieldByName(key.String())
+			mmv := mv.MapIndex(key).Elem()
+			if err := set(mmv, ssv); err != nil {
+				return err
 			}
 		}
 	default:
+		return errors.New("unknown error")
 	}
 	return nil
 }
+
